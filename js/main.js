2262525ba34fb8e4160d6a3b9e00577df1e63db6
Dimas Abimanyu <dmsabmny@gmail.com>
2020-07-01 14:23:47 +0700

    Add responsive navigation menu

--- js/main.js
+++ js/main.js
@@ -33,3 +33,28 @@ button.addEventListener("mouseleave", function() {
   item.style.left = buttonX + "px";
 });
 
+// Navigation
+const navSlide = () => {
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav-links');
+  const navLinks = document.querySelectorAll('.nav-links li');
+
+  
+  burger.addEventListener('click', () => {
+      nav.classList.toggle('nav-active'); //Toggle nav
+      
+      // Animate links
+      navLinks.forEach((link, index) => { 
+          if(link.style.animation){
+              link.style.animation = '';
+          }else{
+              link.style.animation = `navLinkFade 0.5s ease forwards`;
+          }
+          // console.log(index / 5 + 0.2);
+      });
+      // Burger animation
+      burger.classList.toggle('toggle');
+  });
+}
+
+navSlide();
